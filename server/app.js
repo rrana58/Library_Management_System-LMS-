eab05104a47388ae1a883146cd46ff5dbd040b14
rrana58 <rozinar318@gmail.com>
2026-04-19 15:17:25 +0545

    chore(app): sync server configuration and route registrations

--- server/app.js
+++ server/app.js
@@ -10,8 +10,14 @@ import authRouter from "./routes/authRouter.js";
 import bookRouter from "./routes/bookRouter.js";
 import borrowRouter from "./routes/borrowRouter.js";
 import userRouter from "./routes/userRouter.js";
+import notificationRouter from "./routes/notificationRouter.js";
+import chatbotRouter from "./routes/chatbotRouter.js";
+import paymentRouter from "./routes/paymentRouter.js";
+
 import { notifyUsers } from "./services/notifyUsers.js";
 import {removeUnverifiedAccounts} from "./services/removeUnverifiedAccounts.js";
+import {cleanupReservations} from "./services/cleanupReservations.js";
+import {deleteScheduledAccounts} from "./services/deleteScheduledAccounts.js";
 
 export const app = express();
 
@@ -37,19 +43,20 @@ app.use(fileupload({
 
 
 
-app.use((req, res, next) => {
-    console.log("Incoming Content-Type:", req.headers['content-type']);
-    next();
-});
 
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/book", bookRouter);
 app.use("/api/v1/borrow", borrowRouter);
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/notification", notificationRouter);
+app.use("/api/v1/chatbot", chatbotRouter);
+app.use("/api/v1/payment", paymentRouter);
 
 notifyUsers();
 removeUnverifiedAccounts();
+cleanupReservations();
+deleteScheduledAccounts();
 connectDB();
 
 app.use(errorMiddleware);
